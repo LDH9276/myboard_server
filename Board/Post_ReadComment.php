080c8685ae617051dcbe82fafae9eb648168fd9c
LDH9276 <leedh9276@naver.com>
2023-08-24 17:54:41 +0900

    댓글 기능 추가

---
+++ Board/Post_ReadComment.php
@@ -0,0 +1,40 @@
+<?php
+
+// DB 연결
+include_once '../cors.php';
+include_once '../dbconn.php';
+
+// POST로 받아오기
+$id = $_GET['id'] ?? '';
+$id = (int)$id;
+
+// 리스트 출력하기
+$stmt = $conn->prepare("SELECT * FROM app_comment WHERE post_id = ?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+// 리스트 반복처리 후 JSON 데이터로 변환
+$list = array();
+
+while($row = mysqli_fetch_array($result)) {
+  $id = $row['id'] ?? '';
+  $content = $row['content'] ?? '';
+  $writer = $row['writer'] ?? '';
+  $reg_date = $row['reg_date'] ?? '';
+
+  $list[] = array(
+    'id' => $id,
+    'content' => $content,
+    'writer' => $writer,
+    'reg_date' => $reg_date
+  );
+}
+
+// JSON으로 echo 처리하기
+echo json_encode(array("list" => $list));
+
+// DB 처리 종료
+$stmt->close();
+$conn->close();
+?>
---
+++ Board/Post_WriteComment.php
@@ -0,0 +1,61 @@
+<?php
+
+// DB 연결
+include_once '../cors.php';
+include_once '../dbconn.php';
+
+// POST 요청으로 받은 데이터
+// ID   : 아이디
+// data : 글 내용
+// file : 파일
+$writer = $_POST['writer'] ?? '';
+$content = $_POST['content'] ?? '';
+$post_id = $_POST['post_id'] ?? '';
+$reg_date = date("Y-m-d H:i:s");
+
+// 수정모드시 받아올 것들
+$id = $_POST['id'] ?? '';
+$modify = $_POST['modify'] ?? false;
+$delete = $_POST['delete'] ?? false;
+
+// SQL 인젝션 방지
+$writer = mysqli_real_escape_string($conn, $writer);
+$content = mysqli_real_escape_string($conn, $content);
+
+// 수정모드일 경우에는 ($modify == true)
+if ($modify) {
+  // 수정모드
+  $stmt = $conn->prepare("UPDATE app_comment SET content = ?, update_date = ? WHERE id = ?");
+  $update_date = date("Y-m-d H:i:s");
+  $stmt->bind_param("ssi", $content, $update_date, $id);
+  $stmt->execute();
+
+  // DB 처리 종료
+  $stmt->close();
+  $conn->close();
+
+// 삭제모드일 경우에는 ($delete == true)
+} else if (!$modify && $delete) {
+  // 삭제모드
+  $stmt = $conn->prepare("DELETE FROM app_comment WHERE id = ?");
+  $stmt->bind_param("i", $id);
+  $stmt->execute();
+
+  // DB 처리 종료
+  $stmt->close();
+  $conn->close();
+}
+
+// 수정사항이 아닐 경우에는
+else {
+
+// DB에 글 저장
+$stmt = $conn->prepare("INSERT INTO app_comment (post_id, writer, content, reg_date) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $post_id, $writer, $content, $reg_date);
+$stmt->execute();
+
+// DB 처리 종료
+$stmt->close();
+$conn->close();
+}
+?>
